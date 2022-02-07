bc9acda72b32bc0c07267ca4d95021a7b1a8c291
Lakshya Bhasin <75601972+lakshyabhasin509@users.noreply.github.com>
2022-02-07 23:46:50 +0530

    Time: 0 ms (100.00%), Space: 46.1 MB (11.14%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
@@ -1,21 +1,19 @@
 class Solution {
 
     public int peakIndexInMountainArray(int[] arr) {
-
-
-        int start=0;
+int start=0;
         int end=arr.length-1;
-        int mid;
-// using binary search
-        while (start<end){
-            mid=start+((end-start)/2);
-            if (arr[mid]<arr[mid+1])
-            { start=mid+1;
-          }
-            else{
-
-                end=mid;
-            }
-        }
-       return start;
-    }}
+        
+while(start<end)
+{
+    int mid=start+(end-start)/2;
+    if(arr[mid]<arr[mid+1])
+        start=mid+1;
+    else if(arr[mid]>arr[mid+1])
+        end=mid;
+    
+}  
+        return start;
+        
+    }
+}
