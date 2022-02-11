33a436552a0b8ab8ab7ba19fa69186a7b7cc48c1
Lakshya Bhasin <75601972+lakshyabhasin509@users.noreply.github.com>
2022-02-11 23:43:04 +0530

    Time: 2 ms (72.63%), Space: 43.3 MB (13.17%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -1,36 +1,16 @@
-class Solution {
+
+        
+ class Solution {
     public boolean checkIfExist(int[] arr) {
-        
-    Arrays.sort(arr);
-        
-        for(int i=0;i<arr.length;i++){
-            
-                int search=BS(arr,(arr[i]*2));
-            if(search!=-1 && search!=i)
-                return true;
-          
-            
+        for (int i=0;i<arr.length;i++){
+            for (int j=0;j<arr.length;j++){
+                if (arr[i]==(arr[j]*2) && i!=j){
+                    return true;
+                }
+            }
         }
         return false;
     }
-    
-    
-    int BS(int[] arr,int key){
-        int start=0;
-        int end=arr.length-1;
-        
-        while(start<=end){
-            int mid=start+(end-start)/2;
-    
-            if(arr[mid]<key)
-                start=mid+1;
-            else if(arr[mid]>key)
-                end=mid-1;
-            else return mid;
-        }
-        return -1;
-    }
-
-
-
 }
+
+
