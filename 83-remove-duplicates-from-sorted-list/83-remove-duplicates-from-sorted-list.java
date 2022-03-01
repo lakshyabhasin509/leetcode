5ad2734fb970c07cb0e9d248418f2b60b0e1f122
Lakshya Bhasin <75601972+lakshyabhasin509@users.noreply.github.com>
2022-03-01 12:03:26 +0530

    Time: 1 ms (71.19%), Space: 44 MB (30.48%) - LeetHub

--- 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.java
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.java
@@ -8,6 +8,8 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// recursive method
+
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
        
