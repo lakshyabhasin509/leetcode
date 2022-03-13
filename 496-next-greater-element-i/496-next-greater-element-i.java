856fbb03e53266b6824c23c115b1bf1655c722fe
Lakshya Bhasin <75601972+lakshyabhasin509@users.noreply.github.com>
2022-03-13 11:27:53 +0530

    Time: 5 ms (62.42%), Space: 44.4 MB (28.19%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -1,32 +1,32 @@
 class Solution {
+//  Optimized using Stack.
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-       
-        
-        for(int i=0;i<nums1.length;i++){
-             Stack<Integer> stack=new Stack<Integer>();
-            for(int j=nums2.length-1;j>=0;j--){
-               
-                int ans;
-                if(stack.isEmpty()){
-                    ans=-1;
-                }
-                else {
-                    if(stack.peek()>nums2[j])
-                        ans=stack.peek();
-                    else{
-                        while(!stack.isEmpty() && stack.peek()<=nums2[j]){
-                            stack.pop();
-                            
-                        }
-                        if(stack.isEmpty())ans=-1;
-                        else ans=stack.peek();
-                    }
-                }
-                stack.push(nums2[j]);
-                if(nums1[i]==nums2[j]){
-                    nums1[i]=ans;
-                break;}
+        Stack<Integer> s  = new Stack<>();
+        int[] ans = new int[nums1.length];
+        Map<Integer,Integer> m = new HashMap<>();
+        for(int i=nums2.length-1;i>=0;i--){
+            if(s.isEmpty()){
+                m.put(nums2[i],-1);
             }
+            else if(s.peek()>nums2[i]){
+                m.put(nums2[i],s.peek());
+            }
+            else{
+                while(!s.isEmpty() && s.peek()<=nums2[i]){
+                    s.pop();
+                }
+                if(s.isEmpty()){
+                    m.put(nums2[i],-1);
+                }
+                else{
+                    m.put(nums2[i],s.peek());
+                }
+            }
+            s.push(nums2[i]);
         }
-   return nums1; }
+        for(int i=0;i<nums1.length;i++){
+            ans[i] = m.get(nums1[i]);
+        }
+        return ans;
+    }
 }
