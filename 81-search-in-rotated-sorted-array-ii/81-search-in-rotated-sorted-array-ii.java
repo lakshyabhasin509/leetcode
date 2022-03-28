40c0b0e40a70e565b7500b67f045b89b4b5eb6e9
Lakshya Bhasin <75601972+lakshyabhasin509@users.noreply.github.com>
2022-03-28 08:00:09 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (79.96%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
@@ -1,79 +1,27 @@
 class Solution {
-    public boolean search(int[] nums, int target) {
+    
+public boolean search(int[] nums, int target) {
+        return search(nums,target,0,nums.length-1);
+    }
+    public boolean search(int[] nums,int target,int start,int end){
+        if(start>end)return false;
+        if(nums[start]==nums[end]){
+            if(nums[start]==target)
+                return true;
+            return search(nums,target,start+1,end-1);
+        }
+        int mid=start+(end-start)/2;
         
-        int pivot = findPivot(nums);
-
-        // if you did not find a pivot, it means the array is not rotated
-        if (pivot == -1) {
-            
-            int BS=binarySearch(nums, target, 0 , nums.length - 1);
-                if(BS!=-1)
-                    return true;
-            else return false;
+        if(nums[mid]==target)return true;
+        if(nums[mid]>=nums[start])
+        {
+            if(target>=nums[start] && target<=nums[mid])
+                return search(nums,target,start,mid-1);
+            else return search(nums,target,mid+1,end);
         }
-
-        // if pivot is found, you have found 2 asc sorted arrays
-        if (nums[pivot] == target) {
-            return true;
-        }
-
-        if (target >= nums[0]) {
-            int BS=binarySearch(nums, target, 0, pivot - 1);
-        if(BS!=-1)
-                    return true;
-            else return false;
-        }
-
-        else if(binarySearch(nums, target, pivot + 1, nums.length - 1)!=-1)
-            return true;
-             else return false;
-    }
-
-    int binarySearch(int[] arr, int target, int start, int end) {
-        while(start <= end) {
-            // find the middle element
-//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
-            int mid = start + (end - start) / 2;
-
-            if (target < arr[mid]) {
-                end = mid - 1;
-            } else if (target > arr[mid]) {
-                start = mid + 1;
-            } else {
-                // ans found
-                return mid;
-            }
-        }
-        return -1;
-    }
-// for duplicates
-     int findPivot(int[] arr) {
-        int start = 0;
-        int end = arr.length - 1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            // 4 cases over here
-            if (mid < end && arr[mid] > arr[mid + 1]) {
-                return mid;
-            }
-            if (mid > start && arr[mid] < arr[mid - 1]) {
-                return mid-1;
-            }
-            if(arr[mid]==arr[start] && arr[start]==arr[end])
-            {
-                if(start<end && arr[start]>arr[start+1])
-                    return start;
-                start=start+1;
-            
-                if(end >start && arr[end]<arr[end-1])
-                    return end;
-                end--;
-                
-            }
-            else if(arr[start]<arr[mid] ||(arr[mid]==arr[start]&& arr[end]<arr[start]))
-                start=mid+1;
-            else end=mid-1;
-            
-        }
-        return -1;
+        if(target>=nums[mid]&& target<=nums[end])
+            return search(nums,target,mid+1,end);
+        
+        
+         return search(nums,target,start,mid-1);
     }}
