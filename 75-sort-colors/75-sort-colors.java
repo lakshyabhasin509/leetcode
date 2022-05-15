01989bd47fe6800a3ba69baee295f6ae7392f206
Lakshya Bhasin <75601972+lakshyabhasin509@users.noreply.github.com>
2022-05-15 14:26:21 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (20.61%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,5 +1,18 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+       int [] colors=new int[3];
+        for(int i :nums){
+            colors[i]++;
+        }
+        int k=0;
+        for(int i=0;i<nums.length;i++){
+            while(k<3 && colors[k]==0)k++;
+            if(colors[k]!=0){
+                nums[i]=k;
+                colors[k]--;
+            }
+            
+            
+        }
     }
 }
