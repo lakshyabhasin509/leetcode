ba2db15da30324d1edf7f3b185a0de2e7aa3011f
Lakshya Bhasin <75601972+lakshyabhasin509@users.noreply.github.com>
2022-05-19 21:14:59 +0530

    Added solution - LeetHub

---
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.java
@@ -0,0 +1,61 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class DriverClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+            int V = sc.nextInt();
+            int E = sc.nextInt();
+            for (int i = 0; i < V + 1; i++)
+                list.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                int u = sc.nextInt();
+                int v = sc.nextInt();
+                list.get(u).add(v);
+            }
+            if (new Solution().isCyclic(V, list) == true)
+                System.out.println("1");
+            else
+                System.out.println("0");
+        }
+    }
+}// } Driver Code Ends
+
+
+/*Complete the function below*/
+
+class Solution {
+    // Function to detect cycle in a directed graph.
+    public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
+             boolean vis[] = new boolean[V];
+            HashSet<Integer> parent=new HashSet<>();
+
+            for (int i = 0; i < V; i++) {
+                if (!vis[i]) {
+                    if (DFSCycleDirected(i, adj, vis, parent)) return true;
+                }
+            }
+
+            return false;
+        }
+        public boolean DFSCycleDirected(int v, ArrayList<ArrayList<Integer>> adj,
+                                        boolean[] vis, HashSet<Integer> parent){
+            vis[v]=true;
+            parent.add(v);
+            for (int i:adj.get(v)) {
+                if (!vis[i]){
+                    if (DFSCycleDirected(i,adj,vis,parent))
+            return true;    }
+                else if (parent.contains(i))return true;
+
+            }
+            parent.remove(v);
+        return false;
+        }
+}
