bafff04873cdf2eba0dc5e1ef2327ce15ae84aaf
Lakshya Bhasin <75601972+lakshyabhasin509@users.noreply.github.com>
2022-05-31 20:55:57 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (39.62%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,7 +1,14 @@
 class Solution {
     public int fib(int n) {
-        if(n<2)return n;
-        
-        return fib(n-1)+fib(n-2);
-    }
-}
+      int t[]=new int[n+1];
+        if(n<=1)return n;
+        t[0]=0;
+        t[1]=1;
+
+        for(int i=2;i<n+1;i++){
+            t[i]=t[i-1]+t[i-2];
+        }
+
+        return t[n];
+    
+    }}
