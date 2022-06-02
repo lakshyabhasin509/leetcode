617a83a7156c9774d8c927b3c4c26b002f5361ba
Lakshya Bhasin <75601972+lakshyabhasin509@users.noreply.github.com>
2022-06-02 22:31:28 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (30.16%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,24 +1,24 @@
 class Solution {
-    int dp[];
     
     public int rob(int[] nums) {
-        dp=new int[nums.length];
-        dp[0]=nums[0];
         return solve(nums,nums.length);
    
 }
     int solve(int[]arr,int n){
-      
+      int prevprev=arr[0];
+        int prev=prevprev;
         for(int i=1;i<n;i++){
            int accept=arr[i];
             if(i>=2)
-             accept=dp[i-2]+arr[i];
-            int reject=dp[i-1];
+            accept=prevprev+arr[i];
+            int reject=prev;
             
-            dp[i]=Math.max(accept,reject);
+           int cur=Math.max(accept,reject);
+            prevprev=prev;
+            prev=cur;
             
         }
-        return dp[n-1];
+        return prev;
     }
     
 }
