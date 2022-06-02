f75e8d0501bfc27bc97450b585675b651300152a
Lakshya Bhasin <75601972+lakshyabhasin509@users.noreply.github.com>
2022-06-02 22:27:29 +0530

    Time: 0 ms (100.00%), Space: 42 MB (19.13%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,24 +1,24 @@
 class Solution {
     int dp[];
-    void memset(){
-        for(int i=0;i<dp.length;i++)
-            dp[i]=-1;
-    }
+    
     public int rob(int[] nums) {
-        dp=new int[nums.length+1];
-        memset();
-        return solve(nums,nums.length-1);
+        dp=new int[nums.length];
+        dp[0]=nums[0];
+        return solve(nums,nums.length);
    
 }
     int solve(int[]arr,int n){
-        if(n==0)return arr[n];
-        if(n<0)return 0;
-        
-        if(dp[n]!=-1)return dp[n];
-        int accept=solve(arr,n-2)+arr[n];
-        int reject=solve(arr,n-1);
-        
-        return dp[n]= Math.max(accept,reject);
+      
+        for(int i=1;i<n;i++){
+           int accept=arr[i];
+            if(i>=2)
+             accept=dp[i-2]+arr[i];
+            int reject=dp[i-1];
+            
+            dp[i]=Math.max(accept,reject);
+            
+        }
+        return dp[n-1];
     }
     
 }
