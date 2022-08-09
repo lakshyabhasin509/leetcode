62125337a8e31e56992555b74001be27be73bd89
Lakshya Bhasin <75601972+lakshyabhasin509@users.noreply.github.com>
2022-08-09 14:58:44 +0530

    Time: 4 ms (55.43%), Space: 47.1 MB (70.69%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -1,4 +1,6 @@
-class Solution {    
+class Solution {
+    int count;
+    
     public int maxAreaOfIsland(int[][] grid) {
         
         int row=grid.length,col=grid[0].length;
@@ -7,28 +9,29 @@ class Solution {
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
                 if(grid[i][j]==1 && !vis[i][j] ){
-                   int ans= dfs(i,j,vis,grid);
-                    area=Math.max(area,ans);
+                    count=0;
+                    dfs(i,j,vis,grid);
+                    if(area<count){
+                        area=count;
+                    }
                 }
             }
         }return area;
     }
     
-    int dfs(int row,int col,boolean vis[][],int [][]grid){
-        if(row<0 || col<0||row>=grid.length || col>=grid[0].length)return 0 ;
-        if(grid[row][col]==0)return 0;
-        if(vis[row][col])return 0;
+    void dfs(int row,int col,boolean vis[][],int [][]grid){
+        if(row<0 || col<0||row>=grid.length || col>=grid[0].length)return;
+        if(grid[row][col]==0)return;
+        if(vis[row][col])return;
         
         
         vis[row][col]=true;
+        count++;
         
-        
-        int left=dfs(row+1,col,vis,grid);
-        int right=dfs(row-1,col,vis,grid);
-       int up= dfs(row,col-1,vis,grid);
-       int down= dfs(row,col+1,vis,grid);
-        
-        return 1+left+right+up+down;
+        dfs(row+1,col,vis,grid);
+        dfs(row-1,col,vis,grid);
+        dfs(row,col-1,vis,grid);
+        dfs(row,col+1,vis,grid);
        
     }
     
