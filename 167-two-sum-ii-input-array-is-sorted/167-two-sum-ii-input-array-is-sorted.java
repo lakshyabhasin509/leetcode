9f09b1c688a005329d3a81132607e79536422ef2
Lakshya Bhasin <75601972+lakshyabhasin509@users.noreply.github.com>
2022-08-25 13:28:57 +0530

    Time: 1 ms (99.47%), Space: 44.9 MB (96.45%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
@@ -9,8 +9,19 @@ class Solution {
             
         int sum=numbers[start]+numbers[end];
         if(sum==target)return new int[]{start+1,end+1};
-        else if(sum>target)end--;
-        else start++;
+        else if(sum>target){
+            if(target<(numbers[start]+numbers[mid]))
+                end=mid-1;
+            else if(target==(numbers[start]+numbers[mid]))return new int[]{start+1,mid+1};
+            else end--;
+        }
+        else{
+           if(target>(numbers[end]+numbers[mid])){
+               start=mid+1;
+           }
+            else if(target==(numbers[end]+numbers[mid]))return new int[]{mid+1,end+1};
+            else start++;
+        }
         }
         return new int[2];
     }
